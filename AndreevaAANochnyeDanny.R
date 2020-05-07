ec4c9bd6fe14ddedab4fedd856acca5d3ffe32a0
Anastasia225 <64834351+Anastasia225@users.noreply.github.com>
2020-05-07 13:03:49 +0300

    Add files via upload

---
+++ AndreevaAANochnyeDanny.R
@@ -0,0 +1,41 @@
+# јндреева јнастаси€ јлександровна - дл€ ночных данных за осень 2019 года, 
+постройте регрессионную зависимость скорости сокотечени€ (переменна€ Flux) дл€ деревьев вида Larix sibirica
+
+
+data = read.csv("data.csv")
+summary(data)
+
+data_filtered_1 = data[data$doy > 243 & data$doy < 335,]
+data_filtered_2 = data_filtered_1[data_filtered_1$hour > 19 | data_filtered_1$hour < 7,]
+data_filtered_3 = data_filtered_2[data_filtered_2$Species == "Larix sibirica",]
+
+data_filtered_3=data_filtered_3[ ,-c("id", "Species","age_group_index","time", "antrop_load", 
+                                     "in_site_antrop_load")]
+library(dplyr)
+
+data_filtered_3=select(data_filtered_3,-c("id", "Species","age_group_index","time", "antrop_load", 
+                                          "in_site_antrop_load") )
+
+
+install.packages("ggcorrplot")
+corr = cor(data_filtered_3,use = "na.or.complete")^2
+##corr=corr[corr>0.49]
+#install.packages("ggcorrplot")
+library(ggcorrplot)
+
+
+ggcorrplot(corr,
+           tl.cex=4
+)
+flux_corr=corr[ ,"Flux"]
+flux_corr=flux_corr[flux_corr>0.1]
+#cor.test(data$flux, data$t1)
+
+formula8 = Flux ~ u+rh
+formula9 = Flux ~ u+rh + u:rh
+
+model8 = lm(data=data_filtered_4, formula8)
+model9 = lm(data=data_filtered_4, formula9)
+
+summary(model8)
+summary(model9)
